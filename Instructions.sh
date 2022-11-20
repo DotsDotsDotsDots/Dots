17d84175f13772fb8ba6cfa5c35a9e2db9e35f2d
Anonymous <117112914+SlipSneed@users.noreply.github.com>
2022-11-20 08:10:49 +0530

    Add files via upload

---
+++ Instructions.sh
@@ -0,0 +1,101 @@
+# Install AppStore Apps:
+1)Stockfish
+2)DropZone
+3)Amphetamine
+4)Encrypto
+5)HextEdit
+6)KaBlock
+7)UserScripts
+8)XCode
+9)Vectornator
+10)Orbot
+11)Transmit
+12)Terminus
+13)TestFlight
+14)Codye
+
+# Install Third Party Apps:
+1)Discord
+2)Keybase
+3)Docker
+4)BurpSuite
+5)TinkerTool
+
+# Install Xcode Command Line Tools:
+sudo xcode-select --install
+sudo xcodebuild -license accept
+
+# Install Brew:
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Formulae:
+brew install asciinema bat coreutils fd fontconfig fzf gh gnupg git go lsd m-cli mas neofetch neovim nmap node pv radare2 ripgrep shellcheck starship thefuck tmux telegram tree vim wget alfred alt-tab amethyst android-studio appcleaner cheatsheet element fig hyper insomnia maccy minecraft neovide numi obsidian onyx postman session spotify steam topnotch visual-studio-code hiddenbar google-chrome zsh-async zsh-autosuggestions zsh-completions zsh-fast-syntax-highlighting zsh-git-prompt zsh-history-substring-search zsh-lovers zsh-navigation-tools zsh-vi-mode zsh-you-should-use
+
+# Install Casks:
+brew tap homebrew/cask-fonts && brew install --cask font-victor-mono-nerd-font
+brew tap railwaycat/emacsmacport && brew install emacs-mac --with-modules
+brew install --cask brooklyn --no-quarantine
+
+# Install Nix:
+sh <(curl -L https://nixos.org/nix/install)
+
+# Install OhMyZSH:
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install RustUp:
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install SpaceShip:
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# Install NVChad:
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+
+# Install AwesomeVim:
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+cd ~/.vim_runtime
+git reset --hard
+git clean -d --force
+git pull --rebase
+pip3 install requests
+python3 ~/.vim_runtime/update_plugins.py
+cp ~/Dots/Configs/Vim/*.vim ~/.vim_runtime/vimrcs/.
+cd ~/
+
+# Install Doom:
+git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
+~/.emacs.d/bin/doom install
+mv /opt/homebrew/opt/emacs-mac/Emacs.app /Applications/Emacs.app
+cp ~/Dots/Configs/Doom/*.el ~/.doom.d/.
+~/.emacs.d/bin/doom sync
+~/.emacs.d/bin/doom upgrade
+~/.emacs.d/bin/doom doctor
+
+# Install PowerLine Fonts:
+git clone https://github.com/powerline/fonts.git --depth=1 ~/Fonts
+chmod +x ~/Fonts/install.sh
+~/Fonts/install.sh
+rm -rf ~/Fonts
+
+# Install Flutter:
+mkdir ~/Developer
+git clone https://github.com/flutter/flutter.git -b stable ~/Developer/Flutter
+sudo gem install ffi -- --enable-libffi-alloc
+sudo gem install cocoapods
+~/Developer/Flutter/bin/flutter doctor --android-licenses
+~/Developer/Flutter/bin/flutter doctor
+
+# Install Astro:
+mkdir ~/Developer/Site
+cd ~/Developer/Site
+npm create astro@latest -- --template blog
+npm cache clean –-force
+
+# Setup DotFiles:
+cp ~/Dots/Configs/.gitconfig ~/.gitconfig
+cp ~/Dots/Configs/.hyper.js ~/.hyper.js
+cp ~/Dots/Configs/.zprofile ~/.zprofile
+cp ~/Dots/Configs/.zshrc ~/.zshrc
+cp ~/Dots/Configs/starship.toml ~/.config/starship.toml
