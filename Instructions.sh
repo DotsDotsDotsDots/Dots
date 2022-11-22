c63ee49afca9d17c2cc444391fd01be346ed84ff
Anonymous <117112914+DotsDotsDots@users.noreply.github.com>
2022-11-22 20:29:23 +0530

    Update Instructions.sh

--- Instructions.sh
+++ Instructions.sh
@@ -97,10 +97,15 @@ cd ~/Developer/Site
 npm create astro@latest -- --template blog
 npm cache clean –-force
 
-# Setup GitHub CLI
+# Setup SSH Key:
+ssh-keygen -t ed25519 -C "@iCloud.com"
+eval "$(ssh-agent -s)"
+ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+pbcopy < ~/.ssh/id_ed25519.pub
+
+# Setup GitHub CLI:
 gh auth login
 gh auth status
-gh ssh-key add ~/.ssh/id_ed25519.pub
 
 # Setup DotFiles:
 cp ~/Dots/Configs/config ~/.ssh/config
